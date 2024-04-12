5973ee8deff142158cc521524bd1d2c49d81005b
KandikondaShivani <156292076+KandikondaShivani@users.noreply.github.com>
2024-04-12 00:44:26 -0500

    Pushing code of LabAssignment 13- Part 2 added passportand account.js

--- app.js
+++ app.js
@@ -3,8 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var dreamdestination = require("./models/dreamdestination");
+// passport config
+// Use the existing connection
+// The Account model 
+var Account =require('./models/account');
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
@@ -15,8 +43,9 @@ mongoose.connect(connectionString);
 var db = mongoose.connection;
 //Bind connection to error event 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")})
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+})
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -36,6 +65,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -47,12 +84,12 @@ app.use('/resource', resourceRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -64,36 +101,39 @@ app.use(function(err, req, res, next) {
 
 // We can seed the collection if needed on server start
 
-async function recreateDB(){
- // Delete everything
- await dreamdestination.deleteMany();
+async function recreateDB() {
+  // Delete everything
+  await dreamdestination.deleteMany();
 
-let instance1 = new 
-dreamdestination({destinationname: "Maldives", location: "South Asia", rating: 4.9 });
- instance1.save().then(doc=>{
- console.log("First object saved")}
- ).catch(err=>{
- console.error(err)
- });
+  let instance1 = new
+    dreamdestination({ destinationname: "Maldives", location: "South Asia", rating: 4.9 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
- let instance2 = new 
-dreamdestination({destinationname: "Bora Bora", location: "France", rating: 4.8});
- instance2.save().then(doc=>{
- console.log("Second object saved")}
- ).catch(err=>{
- console.error(err)
- });
+  let instance2 = new
+    dreamdestination({ destinationname: "Bora Bora", location: "France", rating: 4.8 });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
- let instance3 = new 
-dreamdestination({destinationname: "Maui", location: "USA", rating: 4.7});
- instance3.save().then(doc=>{
- console.log("Third object saved")}
- ).catch(err=>{
- console.error(err)
- });
+  let instance3 = new
+    dreamdestination({ destinationname: "Maui", location: "USA", rating: 4.7 });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 
 }
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 module.exports = app;
